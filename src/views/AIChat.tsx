9e925b377d5f4fd2bb6732e82ffa1a404c597dfb
Jono Brandel <developer@jono.fyi>
2024-09-05 09:20:54 -0700

    Scaffold AI Chat Bot View

--- src/views/AIChat.tsx
+++ src/views/AIChat.tsx
@@ -4,19 +4,19 @@ import cn from 'classnames';
 import { AppDelegate } from '../components/Live2D/AppDelegate';
 import * as AppDefine from '../components/Live2D/AppDefine';
 import { AppGLManager } from '../components/Live2D/AppGLManager';
+import { AppModel, LoadStep } from '../components/Live2D/AppModel';
 import Live2DCubismCore from '@live2d/core/live2dcubismcore.js?url';
 
 import styles from './DefaultLive2D.module.css';
 import { AppLive2DManager } from '../components/Live2D/AppLive2DManager';
 
-export default function View(props: { model?: string; playing?: boolean }) {
+export default function View() {
   const domElement = useRef<HTMLDivElement | null>(null);
+  const refs = useRef<{ model: AppModel | null }>({ model: null });
 
   useEffect(mount, []);
-  useEffect(load, [props.model]);
 
   function mount() {
-    AppDefine.setModelDir('mode');
     if (!document.head.querySelector('#live2dcubism')) {
       const script = document.createElement('script');
       script.id = 'live2dcubism';
@@ -29,21 +29,57 @@ export default function View(props: { model?: string; playing?: boolean }) {
 
     return unmount;
 
-    function setup() {
+    async function setup() {
       if (
         !AppGLManager.getInstance() ||
         !AppDelegate.getInstance().initialize()
       ) {
         return;
       }
-  
+
       window.addEventListener('resize', resize);
 
       if (AppGLManager.canvas && domElement.current) {
         domElement.current.appendChild(AppGLManager.canvas);
       }
-  
+
       AppDelegate.getInstance().run();
+      const manager = AppLive2DManager.getInstance();
+      const model = manager.getModel(0);
+
+      // Model is loaded
+      await ready(model);
+
+      // 12: "ModeB_panel3"
+      // 13: "ModeB_arms2"
+      // 53: "arms"
+      // 45: "functionkey"
+      const cubismModel = model.getModel();
+
+      cubismModel.setPartOpacityByIndex(12, 0);
+      cubismModel.setPartOpacityByIndex(13, 0);
+      cubismModel.setPartOpacityByIndex(53, 0);
+      cubismModel.setPartOpacityByIndex(45, 0);
+
+      // 116: "ParamKeyboardPosition"
+      cubismModel.loadParameters();
+      cubismModel.setParameterValueByIndex(116, 1);
+      cubismModel.saveParameters();
+
+      refs.current.model = model;
+    }
+
+    async function ready(model: AppModel) {
+      if (!!model.getModel() && model._state === LoadStep.CompleteSetup) {
+        return true;
+      } else {
+        await sleep(100);
+        return ready(model);
+      }
+    }
+
+    function sleep(millis: number) {
+      return new Promise((resolve) => setTimeout(resolve, millis));
     }
 
     function unmount() {
@@ -62,9 +98,30 @@ export default function View(props: { model?: string; playing?: boolean }) {
     }
   }
 
-  function load() {
-    // TODO: Load Live2D Cubism Model
-  }
+  function send() {}
 
-  return <div className={cn(styles.view)} ref={domElement} />;
+  return (
+    <div>
+      <div ref={domElement} className={styles.view} />
+      <div
+        className={cn(
+          'fixed bottom-4 left-4 right-4',
+          'flex flex-nowrap flex-row',
+          'items-center gap-4',
+          'max-w-[800px] mx-auto'
+        )}
+      >
+        <textarea
+          className={cn('grow', 'p-4', 'border-stone-300 border')}
+          placeholder="Message AI..."
+        />
+        <button
+          className={cn('min-w-24', 'inline-block p-4', 'bg-stone-200')}
+          onClick={send}
+        >
+          Send
+        </button>
+      </div>
+    </div>
+  );
 }
