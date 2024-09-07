15d6331280ccdb900b64e839a0826bd323eb959a
Jono Brandel <developer@jono.fyi>
2024-09-06 17:35:03 -0700

    Add Model Logging for searching for ArtMesh

--- src/views/VideoTexture.tsx
+++ src/views/VideoTexture.tsx
@@ -5,6 +5,7 @@ import { AppDelegate } from '../components/Live2D/AppDelegate';
 import * as AppDefine from '../components/Live2D/AppDefine';
 import { AppGLManager } from '../components/Live2D/AppGLManager';
 import Live2DCubismCore from '@live2d/core/live2dcubismcore.js?url';
+import { AppModel, LoadStep } from '../components/Live2D/AppModel';
 
 import styles from './DefaultLive2D.module.css';
 import { AppLive2DManager } from '../components/Live2D/AppLive2DManager';
@@ -29,21 +30,40 @@ export default function View(props: { model?: string; playing?: boolean }) {
 
     return unmount;
 
-    function setup() {
+    async function setup() {
       if (
         !AppGLManager.getInstance() ||
         !AppDelegate.getInstance().initialize()
       ) {
         return;
       }
-  
+
       window.addEventListener('resize', resize);
 
       if (AppGLManager.canvas && domElement.current) {
         domElement.current.appendChild(AppGLManager.canvas);
       }
-  
+
       AppDelegate.getInstance().run();
+      const manager = AppLive2DManager.getInstance();
+      const model = manager.getModel(0);
+
+      await ready(model);
+
+      console.log(model);
+    }
+
+    async function ready(model: AppModel) {
+      if (!!model.getModel() && model._state === LoadStep.CompleteSetup) {
+        return true;
+      } else {
+        await sleep(100);
+        return ready(model);
+      }
+    }
+
+    function sleep(millis: number) {
+      return new Promise((resolve) => setTimeout(resolve, millis));
     }
 
     function unmount() {
